cf279e45d6e286ddbb4dd84d88d1b120922b5736
Manuel Felipe Del Toro Salazar <deltorosalazar@gmail.com>
2018-08-26 15:09:39 -0500

    Users seed updated

--- db/seeds/02_users.js
+++ db/seeds/02_users.js
@@ -2,11 +2,11 @@ exports.seed = (knex, Promise) => {
   return knex('users').del()
     .then(() => {
       return knex('users').insert([
-        { name: 'Christian', last_name: 'Avendaño', email: 'cavendano@belatrixsf.com', team_id: '1' },
-        { name: 'Marcos', last_name: 'Alvarez', email: 'malvarez@belatrixsf.com', team_id: '2' },
-        { name: 'Juan Camilo', last_name: 'Herrera', email: 'jcherrera@belatrixsf.com', team_id: '3' },
-        { name: 'Jhonnatan', last_name: 'Cruz', email: 'jdcruz@belatrixsf.com', team_id: '4' },
-        { name: 'Manuel', last_name: 'Del Toro', email: 'mfdeltoro@belatrixsf.com', team_id: '5' }
+        { name: 'Christian', last_name: 'Avendaño', email: 'cavendano@youremail.com', team_id: '1' },
+        { name: 'Marcos', last_name: 'Alvarez', email: 'malvarez@youremail.com', team_id: '2' },
+        { name: 'Juan Camilo', last_name: 'Herrera', email: 'jcherrera@youremail.com', team_id: '3' },
+        { name: 'Jhonnatan', last_name: 'Cruz', email: 'jdcruz@youremail.com', team_id: '4' },
+        { name: 'Manuel', last_name: 'Del Toro', email: 'mfdeltoro@youremail.com', team_id: '5' }
       ])
     })
 }
